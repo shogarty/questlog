7a8364636e29af6f7ae4be1262ea8eb1302bbd8f
Sean Hogarty <shogarty@hawk.iit.edu>
2024-06-06 17:42:47 -0500

    preliminary admin and user site

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Game, Character, Quest
 
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_games = Game.objects.all().count()
+    num_characters = Character.objects.all().count()
+    num_quests = Quest.objects.all().count()
+
+    # The 'all()' is implied by default.
+
+    context = {
+        'num_games': num_games,
+        'num_characters':num_characters,
+        'num_quests':num_quests,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
