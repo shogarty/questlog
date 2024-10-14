f5ccb4813e81cc42e108cb0287f1ee0c2701a35b
Sean Hogarty <shogarty@hawk.iit.edu>
2024-10-14 15:06:56 -0500

    made some progress. CampaignCreate has issues with queryset?

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('mycampaigns/', views.UserCurrentCampaignsView.as_view(), name = 'my-campaigns'),
     path('campaign/<int:pk>', views.CampaignDetailView.as_view(), name='campaign-detail'),
     path('character/<int:pk>', views.CharacterDetailView.as_view(), name='character-detail'),
-    path('quest/<int:pk>', views.QuestDetailView.as_view(), name='quest-detail')
+    path('quest/<int:pk>', views.QuestDetailView.as_view(), name='quest-detail'),
+    path('campaign/create/', views.CampaignCreate.as_view(), name='campaign-create'),
+    path('campaign/<int:pk>/edit/', views.CampaignUpdate.as_view(), name='campaign-update')
 ]
